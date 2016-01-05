c426a6cf7613480b4b90b51b429e1b19b138cff8
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-05 09:35:00 -0600

    Added a test to make sure that the register checkout does not change the email address if it has been specified.

--- tests/Magento/Checkout/RegisterNewCustomerCheckoutTest.php
+++ tests/Magento/Checkout/RegisterNewCustomerCheckoutTest.php
@@ -33,4 +33,35 @@ class RegisterNewCustomerCheckoutTest extends AbstractMagentoTestCase
         self::assertNotEquals('test@example.com', $this->getIdentity()->getEmailAddress());
     }
 
+
+    public function testCheckoutWithSpecifiedEmailAddress()
+    {
+        $customer = $this->getIdentity();
+        /* @var $customer \Magium\Magento\Identities\Customer */
+
+        $address = hash_hmac('sha256', uniqid(), '') . '@example.com';
+        $customer->setEmailAddress($address);
+        $customer->setUniqueEmailAddressGenerated(true);
+
+        $theme = $this->getTheme();
+        $this->commandOpen($theme->getBaseUrl());
+        $this->getLogger()->info('Opening page ' . $theme->getBaseUrl());
+        $addToCart = $this->getAction(AddItemToCart::ACTION);
+        /* @var $addToCart \Magium\Magento\Actions\Cart\AddItemToCart */
+
+        $addToCart->addSimpleProductToCartFromCategoryPage();
+        $this->setPaymentMethod('CashOnDelivery');
+        $customerCheckout= $this->getAction(RegisterNewCustomerCheckout::ACTION);
+        /* @var $customerCheckout \Magium\Magento\Actions\Checkout\RegisterNewCustomerCheckout */
+
+        $customerCheckout->execute();
+
+        $orderId = $this->getExtractor(OrderId::EXTRACTOR);
+        $this->getLogger()->info(sprintf('Extracted %s as the order ID', $orderId->getOrderId()));
+        /** @var $orderId OrderId */
+        self::assertNotNull($orderId->getOrderId());
+        self::assertGreaterThan(0, $orderId->getOrderId());
+        self::assertEquals($address, $this->getIdentity()->getEmailAddress());
+    }
+
 }
