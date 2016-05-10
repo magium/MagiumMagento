fa0e4c0fbe2cc9e27b6df6ea3e90247591418432
Kevin Schroeder <kschroeder@mirageworks.com>
2016-05-10 15:51:42 -0500

    Updated the billing address step to allow easy bypassing of specific items

--- lib/Magento/Navigators/Store/Switcher.php
+++ lib/Magento/Navigators/Store/Switcher.php
@@ -2,12 +2,25 @@
 
 namespace Magium\Magento\Navigators\Store;
 
+use Magium\AbstractTestCase;
+use Magium\Actions\WaitForPageLoaded;
+use Magium\Magento\Themes\AbstractThemeConfiguration;
 use Magium\Navigators\InstructionNavigator;
+use Magium\WebDriver\WebDriver;
 
 class Switcher extends InstructionNavigator
 {
 
     const NAVIGATOR = 'Store\Switcher';
+
+    protected $themeConfiguration;
+
+    public function __construct(AbstractTestCase $testCase, WebDriver $webdriver, WaitForPageLoaded $loaded, AbstractThemeConfiguration $themeConfiguration)
+    {
+        parent::__construct($testCase, $webdriver, $loaded);
+        $this->themeConfiguration = $themeConfiguration;
+    }
+
 
     public function switchTo($store)
     {
