933ab43604239d4294aecdbf078fe1e963fee8e2
Kevin Schroeder <kschroeder@mirageworks.com>
2017-02-21 08:51:23 -0600

    Added a wait condition to make sure that the button for the shpping method is clickable.

--- tests/Magento/Checkout/ShippingTest.php
+++ tests/Magento/Checkout/ShippingTest.php
@@ -5,6 +5,9 @@ namespace Tests\Magium\Magento\Checkout;
 use Magium\Magento\AbstractMagentoTestCase;
 use Magium\Magento\Actions\Cart\AddSimpleProductToCart;
 use Magium\Magento\Actions\Checkout\GuestCheckout;
+use Magium\Magento\Actions\Checkout\ShippingMethods\ByName;
+use Magium\Magento\Actions\Checkout\ShippingMethods\NoSuchShippingMethodException;
+use Magium\Magento\MissingInformationException;
 use Magium\Magento\Navigators\Catalog\DefaultSimpleProduct;
 use Magium\Magento\Navigators\Catalog\DefaultSimpleProductCategory;
 
@@ -15,7 +18,7 @@ class ShippingTest extends AbstractMagentoTestCase
     {
         $this->setPaymentMethod('CashOnDelivery');
         $this->setShippingMethod('ByName');
-        $this->assertInstanceOf('Magium\Magento\Actions\Checkout\ShippingMethods\ByName', $this->getShippingMethod());
+        $this->assertInstanceOf(ByName::class, $this->getShippingMethod());
         $this->getShippingMethod()->setName('Fixed');
         $this->commandOpen($this->getTheme()->getBaseUrl());
         $this->getNavigator(DefaultSimpleProductCategory::NAVIGATOR)->navigateTo();
@@ -26,7 +29,7 @@ class ShippingTest extends AbstractMagentoTestCase
 
     public function testShippingThrowsExceptionWhenNameNotSpecified()
     {
-        $this->setExpectedException('Magium\Magento\MissingInformationException');
+        $this->expectException(MissingInformationException::class);
         $this->setPaymentMethod('CashOnDelivery');
         $this->setShippingMethod('ByName');
         $this->commandOpen($this->getTheme()->getBaseUrl());
@@ -38,7 +41,7 @@ class ShippingTest extends AbstractMagentoTestCase
 
     public function testShippingThrowsExceptionWhenElementNotFound()
     {
-        $this->setExpectedException('Magium\Magento\Actions\Checkout\ShippingMethods\NoSuchShippingMethodException');
+        $this->expectException(NoSuchShippingMethodException::class);
         $this->setPaymentMethod('CashOnDelivery');
         $this->setShippingMethod('ByName');
         $this->getShippingMethod()->setName('boogers');
