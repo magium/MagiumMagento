3017d17bc39a1ac20cb94748f2b2007a7f6a776c
Kevin Schroeder <kschroeder@mirageworks.com>
2017-08-02 15:41:49 -0500

    Added check / money order

---
+++ lib/Magento/Actions/Checkout/PaymentMethods/CheckMoneyOrder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Magium\Magento\Actions\Checkout\PaymentMethods;
+
+use Magium\Magento\AbstractMagentoTestCase;
+use Magium\WebDriver\WebDriver;
+
+class CheckMoneyOrder implements PaymentMethodInterface
+{
+
+    const ACTION = 'Checkout\PaymentMethods\CheckMoneyOrder';
+
+    protected $webDriver;
+    protected $testCase;
+
+    public function __construct(
+        WebDriver                   $webDriver,
+        AbstractMagentoTestCase     $testCase
+    ) {
+        $this->webDriver    = $webDriver;
+        $this->testCase     = $testCase;
+    }
+
+    public function getId()
+    {
+        return 'p_method_checkmo';
+    }
+
+    /**
+     * Fills in the payment form, selecting it, if necessary
+     *
+     * @param $requirePayment
+     */
+
+    public function pay($requirePayment)
+    {
+        if ($requirePayment) {
+            $this->testCase->assertElementExists($this->getId());
+        }
+
+        if ($this->webDriver->elementDisplayed($this->getId())) {
+            $element = $this->webDriver->byId($this->getId());
+            $this->webDriver->getMouse()->click($element->getCoordinates());
+        }
+    }
+}
